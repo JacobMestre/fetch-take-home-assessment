fb611f0b87aabde9422aab63d54a849e53326aaf
JacobMestre <berrydayberryday@gmail.com>
2025-04-23 16:44:14 -0700

    Make skeleton for objects

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Starting now
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from typing import List
+import uuid
+
+class Receipt(BaseModel):
+    #retailer
+    #purchaseDate 
+    #purchaseTime
+    #items (min of 1)
+    #total 
+
+class Item(BaseModel):
+    #shortDescription
+    #price
