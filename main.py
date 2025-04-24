1c3200194af75e8ccbb506d755d6bf4b1117a1bf
JacobMestre <berrydayberryday@gmail.com>
2025-04-24 13:40:41 -0700

    Finish last 3 fields for Receipt

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import List
-from datetime import date
+from datetime import date, time
 import uuid
 
 class Receipt(BaseModel):
     retailer: str = Field(..., pattern=r"^[\w\s\-&]+$", description = "The name of the retailer or store the receipt is from")
     purchaseDate: date = Field(..., description = "The date of the purchase printed on the receipt")
-    #purchaseTime
-    # items: 
-    #total 
+    purchaseTime: time = Field(..., description = "The time of the purchase printed on the receipt. 24-hour time expected")
+    items: List[Item] = Field(..., min_items=1)
+    total: str = Field(..., pattern=r"^\\d+\\.\\d{2}$", description = "The amount paid on the receipt") 
 
 class Item(BaseModel):
     #shortDescription
