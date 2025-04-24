0001dc97bc32efcb2a6c96c736a650305432e1a0
JacobMestre <berrydayberryday@gmail.com>
2025-04-24 13:46:24 -0700

    Complete fields for Item object

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from typing import List
 from datetime import date, time
 import uuid
 
-class Receipt(BaseModel):
-    retailer: str = Field(..., pattern=r"^[\w\s\-&]+$", description = "The name of the retailer or store the receipt is from")
-    purchaseDate: date = Field(..., description = "The date of the purchase printed on the receipt")
-    purchaseTime: time = Field(..., description = "The time of the purchase printed on the receipt. 24-hour time expected")
-    items: List[Item] = Field(..., min_items=1)
-    total: str = Field(..., pattern=r"^\\d+\\.\\d{2}$", description = "The amount paid on the receipt") 
 
 class Item(BaseModel):
-    #shortDescription
-    #price
+    shortDescription: str = Field(..., pattern=r"^[\w\s\-]+$", description = "The short product description for the item.")
+    price: str = Field(..., pattern = r"^\d+\.\d{2}$", description= "The total price paid for this item.")
+
+class Receipt(BaseModel):
+    retailer: str = Field(..., pattern=r"^[\w\s\-&]+$", description = "The name of the retailer or store the receipt is from.")
+    purchaseDate: date = Field(..., description = "The date of the purchase printed on the receipt.")
+    purchaseTime: time = Field(..., description = "The time of the purchase printed on the receipt. 24-hour time expected.")
+    items: List[Item] = Field(..., min_items=1)
+    total: str = Field(..., pattern=r"^\d+\.\d{2}$", description = "The amount paid on the receipt.") 
+
+
+
+
