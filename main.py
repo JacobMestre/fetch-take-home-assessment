cc2f09e333c9bd5ef71089cf0417dfc7be73a20f
JacobMestre <berrydayberryday@gmail.com>
2025-04-24 13:14:45 -0700

    Import date from datetime and use it for purchaseDate

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import List
+from datetime import date
 import uuid
 
 class Receipt(BaseModel):
     retailer: str = Field(..., pattern=r"^[\w\s\-&]+$", description = "The name of the retailer or store the receipt is from")
-    #purchaseDate 
+    purchaseDate: date = Field(..., description = "The date of the purchase printed on the receipt")
     #purchaseTime
     # items: 
     #total 
